8b465131a908cfc9cef7c7fb6fde324e0bf9c3f9
DaNubCoding <danubcoding@gmail.com>
2024-06-12 08:31:27 -0400

    undo testing & fix boomerange damaing statue

--- Boomerang.java
+++ Boomerang.java
@@ -27,7 +27,7 @@ public class Boomerang extends Projectile {
     public void hit() {
         if (!hitCooldown.ended()) return;
 
-        Damage dmg = new Damage(getOwner(), getOwner(), 25, getWorldPos(), 15);
+        Damage dmg = new Damage(getOwner(), this, 25, getWorldPos(), 15);
         getWorld().getDamages().add(dmg);
         for (Entity e : getWorld().getEntitiesInRange(getWorldPos(), 15)) {
             e.physics.applyForce(new Vector2(getWorldRotation()).multiply(5.0));
