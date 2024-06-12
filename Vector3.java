03f4f65127a5e97406019b972d08aa971c0fdffb
LucAsl14 <lucas.fu070807@gmail.com>
2024-06-12 19:11:56 -0400

    fix critical rotateY bug in vector3

--- Vector3.java
+++ Vector3.java
@@ -433,7 +433,7 @@ public class Vector3 {
         double rad = Math.toRadians(angle);
         double cos = Math.cos(rad);
         double sin = Math.sin(rad);
-        return new Vector3(x * cos + z * sin, y, x * sin - z * cos);
+        return new Vector3(x * cos - z * sin, y, x * sin + z * cos);
     }
 
     /**
