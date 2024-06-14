6da858e492fd6fda05f25b104b161dd697a0c8b3
LucAsl14 <lucas.fu070807@gmail.com>
2024-06-14 17:43:07 -0400

    modify skeleton spawning

--- Tombstone.java
+++ Tombstone.java
@@ -12,4 +12,12 @@ public class Tombstone extends EnemySpawner {
     public Tombstone(FeatureData data) {
         super("tombstone", data, Skeleton::new, 1);
     }
+
+    public void update() {
+        if (getWorld().getPlayer().getWorldPos().distanceTo(getWorldPos()) < 80
+        && !getData().containsKey("spawnedEnemies")) {
+            getData().put("spawnedEnemies", null);
+            getWorld().addWorldObject(new Skeleton(), getWorldPos().subtract(new Vector3(0, 10, 0)));
+        }
+    }
 }
