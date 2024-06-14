49aeb41e894344bcf179058db9c71006bda119ee
DaNubCoding <danubcoding@gmail.com>
2024-06-13 21:36:55 -0400

    add title music

--- TitleWorld.java
+++ TitleWorld.java
@@ -6,6 +6,8 @@ public class TitleWorld extends PixelWorld {
     public TitleWorld() {
         super(256, 196);
         addSprite(panorama, getWidth() / 2, getHeight() / 2);
+        Music.set("title_music.wav");
+        Music.play();
     }
 
     @Override
