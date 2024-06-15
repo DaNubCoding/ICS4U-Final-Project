9f341dc8e928025678839d98ae52539514bf4a35
DaNubCoding <danubcoding@gmail.com>
2024-06-15 18:55:22 -0400

    actually fix too many damage particles

--- DamageParticle.java
+++ DamageParticle.java
@@ -16,8 +16,14 @@ public class DamageParticle extends Particle {
         super(getBloodImage(), (int) (Math.random() * 40 + 20));
         physics.applyForce(new Vector2(Math.random() * 360).multiply(Math.random() * 0.7 + 0.5));
         physics.setAffectedByGravity(true);
+    }
+
+    @Override
+    public void addedToWorld(PixelWorld world) {
         loadedParticles.offer(this);
-        if (loadedParticles.size() > 20) loadedParticles.poll();
+        if (loadedParticles.size() > 50) {
+            world.removeSprite(loadedParticles.poll());
+        }
     }
 
     private static GreenfootImage getBloodImage() {
