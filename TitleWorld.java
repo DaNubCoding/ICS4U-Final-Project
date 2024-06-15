0bf256a47ef3cabc8bd768e6827d665994739325
DaNubCoding <danubcoding@gmail.com>
2024-06-15 14:42:19 -0400

    dim title world again

--- TitleWorld.java
+++ TitleWorld.java
@@ -40,6 +40,8 @@ public class TitleWorld extends PixelWorld {
     public void render() {
         GreenfootImage canvas = getCanvas();
         renderSprites();
+        canvas.setColor(new Color(0, 0, 0, 35));
+        canvas.fill();
         canvas.drawImage(title, getWidth() / 2 - title.getWidth() / 2, 30);
     }
 }
