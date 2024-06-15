1296635147720aecd46f4845a0593c852726388b
DaNubCoding <danubcoding@gmail.com>
2024-06-14 22:47:47 -0400

    fix ghost behavior

--- Ghost.java
+++ Ghost.java
@@ -10,10 +10,10 @@ public class Ghost extends Enemy {
     private static final Animation chaseAnimation = new Animation(-1, "ghost_chase");
 
     private final int MAX_HP = 75;
-    private boolean isPhasingIn;
-    private boolean isPhasingOut;
     private Timer moveTimer;
     private Timer invisTimer;
+    private boolean invisible;
+    private Timer attackTimer;
 
     public Ghost() {
         super("ghost_idle");
@@ -27,13 +27,13 @@ public class Ghost extends Enemy {
         physics.setAlwaysTurnTowardsMovement(true);
 
         moveTimer = new Timer(150);
-        invisTimer = new Timer(120);
-        isPhasingIn = false;
-        isPhasingOut = false;
+        invisTimer = new Timer(240);
+        attackTimer = new Timer(25);
     }
 
     @Override
     public void idle(Player player) {
+        setTransparency(255);
         // random movements
         if (moveTimer.ended()) {
             final double distance = Math.random() * 40 + 30;
@@ -45,14 +45,12 @@ public class Ghost extends Enemy {
 
     @Override
     public void notice(Player player) {
-        isPhasingOut = true;
         setLoopingAnimation(chaseAnimation);
     }
 
     @Override
     public void forget(Player player) {
         setLoopingAnimation(idleAnimation);
-        isPhasingIn = true;
     }
 
     @Override
@@ -61,44 +59,28 @@ public class Ghost extends Enemy {
         Vector3 enemyPos = getWorldPos();
         double distanceToPlayer = playerPos.subtract(enemyPos).magnitude();
 
-        // move towards the player as long this is not phasing out
-        if (!isPhasingOut) physics.moveToNearPlayer(1);
-
-        // if at low transparency, teleport and start phasing in
-        if (getTransparency() < 3 && invisTimer.ended()) {
-            isPhasingIn = true;
-            setWorldPos(player.getWorldPos().addXZ(new Vector2(Math.random() * 300 - 150, Math.random() * 300 - 150)));
+        if (invisTimer.ended()) {
+            if (invisible) {
+                setWorldPos(player.getWorldPos().addXZ(new Vector2(Math.random() * 150 - 75, Math.random() * 150 - 75)));
+                showHealth();
+                showShadow();
+            } else {
+                hideHealth();
+                hideShadow();
+            }
+            invisible = !invisible;
+            invisTimer.restart((int) (Math.random() * 300 + 150));
         }
 
-        // if close to player, hit them and start phasing out
-        if (distanceToPlayer <= 25 && !isPhasingIn && !isPhasingOut) {
-            meleePlayer(10, 25);
-            isPhasingOut = true;
-        }
-    }
-
-    public void update() {
-        super.update();
-        // end of phase-in
-        if (getTransparency() < 3) {
-            isPhasingOut = false;
-            invisTimer.restart();
-            hideHealth();
-            hideShadow();
-        }
-        // end of phase-out
-        if (getTransparency() > 252) {
-            isPhasingIn = false;
-            showHealth();
-            showShadow();
-        }
-        // actions to perform while phasing in and out
-        if (isPhasingIn) {
-            setTransparency(getTransparency() + 3);
-        }
-        if (isPhasingOut) {
-            setTransparency(getTransparency() - 3);
-            physics.moveToTarget(getWorldPos().addXZ(new Vector2(getWorldRotation())));
+        if (invisible) {
+            setTransparency(Math.max(0, getTransparency() - 3));
+        } else {
+            setTransparency(Math.min(255, getTransparency() + 6));
+            physics.moveToNearPlayer(10);
+            if (distanceToPlayer < 50 && attackTimer.ended()) {
+                meleePlayer(10, 25);
+                attackTimer.restart();
+            }
         }
     }
 
@@ -106,7 +88,9 @@ public class Ghost extends Enemy {
     public void damage(Damage damage) {
         if (getTransparency() < 100) return;
         super.damage(damage);
-        isPhasingOut = true;
+        if (getHealth() < MAX_HP / 2) {
+            invisTimer.restart(20);
+        }
     }
 
 }
