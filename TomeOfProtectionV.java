ee041dba63503fd24e7c3f4c893b3908c9e7046d
DaNubCoding <danubcoding@gmail.com>
2024-06-16 10:45:27 -0400

    add tome of prot v texture

--- TomeOfProtectionV.java
+++ TomeOfProtectionV.java
@@ -1,5 +1,5 @@
 public class TomeOfProtectionV extends MagicWeapon {
     public TomeOfProtectionV() {
-        super("manual.png", 50, 1, 25, 600, SaintShieldMaker::new);
+        super("tome_of_protection_v.png", 50, 1, 25, 600, SaintShieldMaker::new);
     }
 }
