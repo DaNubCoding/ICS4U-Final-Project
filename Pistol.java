a85cbc700f801d33bc071b8912337067fe20e357
LucAsl14 <94207449+LucAsl14@users.noreply.github.com>
2024-06-16 12:24:12 -0400

    Slightly buff spread of pistol

--- Pistol.java
+++ Pistol.java
@@ -2,11 +2,12 @@
  * A pistol weapon.
  *
  * @author Matthew Li
+ * @author Lucas Fu
  * @version May 2024
  */
 public class Pistol extends RangedWeapon {
     public Pistol() {
-        super("pistol.png", 90, 2, 5, 0, 50, PistolBullet::new);
+        super("pistol.png", 80, 2, 5, 0, 50, PistolBullet::new);
         setCenterOfRotation(new Vector2(3, 7));
     }
 }
